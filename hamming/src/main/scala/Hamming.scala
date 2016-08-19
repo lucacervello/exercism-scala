254e6a8eac9ab9ed8a66f99ed0ee2ba6d3ad6c38
lucacervello <luca.cervello@gmail.com>
2016-08-19 17:01:10 +0200

    Add hamming

---
+++ hamming/src/main/scala/Hamming.scala
@@ -0,0 +1,12 @@
+object Hamming {
+
+  def compute (dna1 : String, dna2 : String) : Int = {
+    (dna1, dna2) match {
+      case ("", "") => 0
+      case ("", dna2) => compute("", dna2.tail)
+      case (dna1, "") => compute(dna1.tail, "")
+      case (dna1, dna2) if (dna1.head == dna2.head) => compute(dna1.tail, dna2.tail)
+      case (dna1, dna2) => 1 + compute(dna1.tail, dna2.tail)
+    }
+  }
+}
