bb9ca68a5a143b0c504b759e2864035bdd9e2c8d
lucacervello <luca.cervello@gmail.com>
2016-12-03 17:44:41 +0100

    Immproved word count

--- word-count/src/main/scala/Phrase.scala
+++ word-count/src/main/scala/Phrase.scala
@@ -1,22 +1,9 @@
-class Phrase (p : String) {
-
-  var phrase = p
-
-  def wordCount() = {
-    wordCountMap(Map(), phrase.split("""[+-.,!@#$%^&*();\/|<>": ]""")
-                   .map(_.toLowerCase())
-                   .filterNot(_ == ""))
-  }
-  // WOrd count non character
-  def wordCountMap(map : Map[String, Int], lsString : Array[String]) : Map[String, Int] = {
-
-    if (lsString.isEmpty) {
-      map
-    } else {
-      map get(lsString.head) match {
-        case Some(c) => wordCountMap(map + (lsString.head -> (c + 1)), lsString.tail)
-        case None => wordCountMap(map + (lsString.head -> 1), lsString.tail)
-      }
-    }
+class Phrase (val phrase : String) {
+  def wordCount = {
+    phrase.split("""[+-.,!@#$%^&*();\/|<>": ]""")
+      .map(_.toLowerCase())
+      .filterNot(_ == "")
+      .groupBy(identity)
+      .mapValues(_.size)
   }
 }
